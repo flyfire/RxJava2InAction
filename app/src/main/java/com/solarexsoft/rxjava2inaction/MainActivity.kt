e368788319017536a3980a40203105a389628314
Solarex <rh.hou.work@gmail.com>
2020-11-17 18:11:47 +0800

    init

---
+++ app/src/main/java/com/solarexsoft/rxjava2inaction/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solarexsoft.rxjava2inaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
